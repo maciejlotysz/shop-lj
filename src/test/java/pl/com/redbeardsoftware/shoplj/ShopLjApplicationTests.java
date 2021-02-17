a0a3972bbb1418061f1feb6a9bc5ce924ec95c62
Maciej Lotysz <maciek.lotysz@gmail.com>
2021-02-17 22:09:21 +0100

    Initial commit

---
+++ src/test/java/pl/com/redbeardsoftware/shoplj/ShopLjApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.redbeardsoftware.shoplj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopLjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
